02c15d8c31c7c301f40dd10a3f519de359f55570
Dmitry <carriingfate92@yandex.ru>
2012-03-08 20:47:01 +0400

    Заготовка

--- src/xml36/mainwindow.cpp
+++ src/xml36/mainwindow.cpp
@@ -41,47 +41,6 @@ void MainWindow::on_actionOpen_triggered() {
 
 bool MainWindow::OpenXML(const QString &fileName) {
   ui->xml_tree->addTopLevelItem(new QTreeWidgetItem(QStringList(fileName)));
-  QFile file(fileName);
-  if (!file.open(QIODevice::ReadOnly))
-      qDebug()<<"Failed!"<<endl;
-  QTextStream stream(&file);
-  QString line = stream.readAll();
-  QString att, val;
-  line.remove(line.indexOf("<?"),line.indexOf("?>")+2);
-      qDebug()<<line<<endl;
-   while (!line.isEmpty())
-    {
-      QString str = line.mid(line.indexOf("<"),line.indexOf(">"));
-      line.remove(str,Qt::CaseInsensitive);
-      bool list= str.contains("/>",Qt::CaseInsensitive); //true, если дошли до листка
-      str.remove("<",Qt::CaseInsensitive).remove(">",Qt::CaseInsensitive);
-      qDebug()<<"str "<<str<<" "<<list<<endl;
-      str.remove("<",Qt::CaseInsensitive).remove(">",Qt::CaseInsensitive); //очищаем строку от ">" и "<"
-        if ((str.indexOf("=")<str.size()) && (str.indexOf("=")>0))
-          {
-            QString buff = str;
-            qDebug()<<"buff "<<buff<<endl;
-            buff.simplified();
-            att = str.remove(str.indexOf("="),str.size()).simplified(); //атрибут - строка от начала до "="
-            qDebug()<<"att"<<att<<endl;
-            ui->attibutes_list->addTopLevelItem(new QTreeWidgetItem(QStringList(att))); //выводим значение атрибута
-            val = buff.remove(att, Qt::CaseInsensitive).simplified();
-            val.remove("='").remove("'").simplified(); //очищаем значение атрибута от лишнего
-            qDebug()<<"val "<<val<<endl;
-            str = att.remove(att.indexOf(" "),att.size());
-            qDebug()<<"deleting element"<<str<<endl;
-            str.simplified();
-            if (!list)
-                line.remove(" </"+str+"> ",Qt::CaseInsensitive);
-            qDebug()<<"line after remove //"<<line<<endl;
-          }
-       else
-         {
-           ui->attibutes_list->addTopLevelItem(new QTreeWidgetItem(QStringList(str))); //случай корня
-           line.remove("</"+str+">",Qt::CaseInsensitive);
-         }
-        qDebug()<<line<<endl;
-    }
    return true;
 }
 

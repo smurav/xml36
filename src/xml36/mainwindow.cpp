2d34994cb9662abd24f7488c4ef0c41d05fa5e1c
DanilinEgor <danegor@gmail.com>
2012-03-10 20:51:32 +0300

    Update src/xml36/mainwindow.cpp

--- src/xml36/mainwindow.cpp
+++ src/xml36/mainwindow.cpp
@@ -42,38 +42,7 @@ void MainWindow::on_actionOpen_triggered() {
 
 bool MainWindow::OpenXML(const QString &fileName)
 {
-    //ui->xml_tree->addTopLevelItem(new QTreeWidgetItem(QStringList(fileName)));
-    QFile file(fileName);
-    file.open(QIODevice::ReadOnly);
-    QTextStream in(&file);
-    while (!in.atEnd())
-    {
-        QString s, s1;
-        in >> s;
-        int n1,n2,k=0;
-        s1=s;
-        if(s.length()!=0 && s.length()!=1 && s[0]=='<')
-        {
-            if(s[1]=='?');
-            else
-            {
-                n1=s.indexOf('<');
-                s.remove(0,n1);
-                n2=s.indexOf(' ');
-                if(n2==-1)
-                    n2=s.indexOf('>');
-                if(s1[n1+1]!='/')
-                {
-                    QTreeWidgetItem* in=new QTreeWidgetItem(QStringList(s1.mid(n1+1,n2-1)));
-                    if(k==0)ui->xml_tree->addTopLevelItem(in);
-                    else ui->xml_tree->insertTopLevelItem(2, in);
-                    if(s1[s1.indexOf('>')-1]!='/')	k++;
-                }
-                else k--;
-            }
-        }
-   }
-   file.close();
+    ui->xml_tree->addTopLevelItem(new QTreeWidgetItem(QStringList(fileName)));
    return true;
 }
 

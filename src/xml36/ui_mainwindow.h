1f17ce416589fc10a8241fa91461812f8ab45bf0
smurav36 <student@kaf36.ru>
2012-03-26 09:25:50 +0400

    меню

--- src/xml36/ui_mainwindow.h
+++ src/xml36/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Sun Mar 25 23:26:36 2012
+** Created: Mon Mar 26 09:22:46 2012
 **      by: Qt User Interface Compiler version 4.8.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
@@ -17,6 +17,7 @@
 #include <QtGui/QHBoxLayout>
 #include <QtGui/QHeaderView>
 #include <QtGui/QMainWindow>
+#include <QtGui/QMenu>
 #include <QtGui/QMenuBar>
 #include <QtGui/QSplitter>
 #include <QtGui/QStatusBar>
@@ -30,12 +31,21 @@ class Ui_MainWindow
 {
 public:
     QAction *actionOpen;
+    QAction *actionNew;
+    QAction *actionSave;
+    QAction *actionSaveAs;
+    QAction *actionAdd;
+    QAction *actionDelete;
+    QAction *actionEdit;
+    QAction *actionExit;
     QWidget *centralWidget;
     QHBoxLayout *horizontalLayout;
     QSplitter *splitter;
     QTreeWidget *xml_tree;
     QTreeWidget *attributes_list;
     QMenuBar *menuBar;
+    QMenu *menu;
+    QMenu *menuXML;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
 
@@ -49,6 +59,35 @@ public:
         QIcon icon;
         icon.addFile(QString::fromUtf8(":/open.png"), QSize(), QIcon::Normal, QIcon::Off);
         actionOpen->setIcon(icon);
+        actionNew = new QAction(MainWindow);
+        actionNew->setObjectName(QString::fromUtf8("actionNew"));
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8(":/new.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionNew->setIcon(icon1);
+        actionSave = new QAction(MainWindow);
+        actionSave->setObjectName(QString::fromUtf8("actionSave"));
+        QIcon icon2;
+        icon2.addFile(QString::fromUtf8(":/save.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionSave->setIcon(icon2);
+        actionSaveAs = new QAction(MainWindow);
+        actionSaveAs->setObjectName(QString::fromUtf8("actionSaveAs"));
+        actionAdd = new QAction(MainWindow);
+        actionAdd->setObjectName(QString::fromUtf8("actionAdd"));
+        QIcon icon3;
+        icon3.addFile(QString::fromUtf8(":/plus.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionAdd->setIcon(icon3);
+        actionDelete = new QAction(MainWindow);
+        actionDelete->setObjectName(QString::fromUtf8("actionDelete"));
+        QIcon icon4;
+        icon4.addFile(QString::fromUtf8(":/minus.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionDelete->setIcon(icon4);
+        actionEdit = new QAction(MainWindow);
+        actionEdit->setObjectName(QString::fromUtf8("actionEdit"));
+        QIcon icon5;
+        icon5.addFile(QString::fromUtf8(":/pencil.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionEdit->setIcon(icon5);
+        actionExit = new QAction(MainWindow);
+        actionExit->setObjectName(QString::fromUtf8("actionExit"));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
         horizontalLayout = new QHBoxLayout(centralWidget);
@@ -71,6 +110,10 @@ public:
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QString::fromUtf8("menuBar"));
         menuBar->setGeometry(QRect(0, 0, 842, 25));
+        menu = new QMenu(menuBar);
+        menu->setObjectName(QString::fromUtf8("menu"));
+        menuXML = new QMenu(menuBar);
+        menuXML->setObjectName(QString::fromUtf8("menuXML"));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
@@ -80,9 +123,27 @@ public:
         statusBar->setObjectName(QString::fromUtf8("statusBar"));
         MainWindow->setStatusBar(statusBar);
 
+        menuBar->addAction(menu->menuAction());
+        menuBar->addAction(menuXML->menuAction());
+        menu->addAction(actionNew);
+        menu->addAction(actionOpen);
+        menu->addAction(actionSave);
+        menu->addAction(actionSaveAs);
+        menu->addSeparator();
+        menu->addAction(actionExit);
+        menuXML->addAction(actionAdd);
+        menuXML->addAction(actionDelete);
+        menuXML->addAction(actionEdit);
+        mainToolBar->addAction(actionNew);
         mainToolBar->addAction(actionOpen);
+        mainToolBar->addAction(actionSave);
+        mainToolBar->addSeparator();
+        mainToolBar->addAction(actionAdd);
+        mainToolBar->addAction(actionDelete);
+        mainToolBar->addAction(actionEdit);
 
         retranslateUi(MainWindow);
+        QObject::connect(actionExit, SIGNAL(triggered()), MainWindow, SLOT(close()));
 
         QMetaObject::connectSlotsByName(MainWindow);
     } // setupUi
@@ -91,11 +152,38 @@ public:
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "XML36", 0, QApplication::UnicodeUTF8));
         actionOpen->setText(QApplication::translate("MainWindow", "\320\236\321\202\320\272\321\200\321\213\321\202\321\214", 0, QApplication::UnicodeUTF8));
+        actionNew->setText(QApplication::translate("MainWindow", "\320\241\320\276\320\267\320\264\320\260\321\202\321\214", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        actionNew->setToolTip(QApplication::translate("MainWindow", "\320\241\320\276\320\267\320\264\320\260\321\202\321\214 XML \320\264\320\276\320\272\321\203\320\274\320\265\320\275\321\202", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        actionSave->setText(QApplication::translate("MainWindow", "\320\241\320\276\321\205\321\200\320\260\320\275\320\270\321\202\321\214", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        actionSave->setToolTip(QApplication::translate("MainWindow", "\320\241\320\276\321\205\321\200\320\260\320\275\320\270\321\202\321\214 XML \320\264\320\276\320\272\321\203\320\274\320\265\320\275\321\202", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        actionSaveAs->setText(QApplication::translate("MainWindow", "\320\241\320\276\321\205\321\200\320\260\320\275\320\270\321\202\321\214 \320\272\320\260\320\272 ...", 0, QApplication::UnicodeUTF8));
+        actionAdd->setText(QApplication::translate("MainWindow", "\320\224\320\276\320\261\320\260\320\262\320\270\321\202\321\214", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        actionAdd->setToolTip(QApplication::translate("MainWindow", "\320\224\320\276\320\261\320\260\320\262\320\270\321\202\321\214", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        actionDelete->setText(QApplication::translate("MainWindow", "\320\243\320\264\320\260\320\273\320\270\321\202\321\214", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        actionDelete->setToolTip(QApplication::translate("MainWindow", "\320\243\320\264\320\260\320\273\320\270\321\202\321\214", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        actionEdit->setText(QApplication::translate("MainWindow", "\320\240\320\265\320\264\320\260\320\272\321\202\320\270\321\200\320\276\320\262\320\260\321\202\321\214", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        actionEdit->setToolTip(QApplication::translate("MainWindow", "\320\240\320\265\320\264\320\260\320\272\321\202\320\270\321\200\320\276\320\262\320\260\321\202\321\214", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        actionExit->setText(QApplication::translate("MainWindow", "\320\222\321\213\321\205\320\276\320\264", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        actionExit->setToolTip(QApplication::translate("MainWindow", "\320\222\321\213\321\205\320\276\320\264", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
         QTreeWidgetItem *___qtreewidgetitem = xml_tree->headerItem();
         ___qtreewidgetitem->setText(0, QApplication::translate("MainWindow", "\320\243\320\267\320\265\320\273", 0, QApplication::UnicodeUTF8));
         QTreeWidgetItem *___qtreewidgetitem1 = attributes_list->headerItem();
         ___qtreewidgetitem1->setText(1, QApplication::translate("MainWindow", "\320\227\320\275\320\260\321\207\320\265\320\275\320\270\320\265", 0, QApplication::UnicodeUTF8));
         ___qtreewidgetitem1->setText(0, QApplication::translate("MainWindow", "\320\220\321\202\321\200\320\270\320\261\321\203\321\202", 0, QApplication::UnicodeUTF8));
+        menu->setTitle(QApplication::translate("MainWindow", "\320\244\320\260\320\271\320\273", 0, QApplication::UnicodeUTF8));
+        menuXML->setTitle(QApplication::translate("MainWindow", "XML", 0, QApplication::UnicodeUTF8));
     } // retranslateUi
 
 };
